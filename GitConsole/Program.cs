9fa7ab68e088d6e16fa3fda7d1cb631502630706
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-01 13:18:36 +0100

    Add project files.

---
+++ GitConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
